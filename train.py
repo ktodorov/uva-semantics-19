95ee05385b8a5c828f846680c0c66ee43c5de33d
ktodorov <kztodorov@outlook.com>
2019-04-22 17:25:15 +0200

    Major refactor of the train implementation
     - Now loads the batches again every epoch
     - Dev evaluation is moved after all training data has passed
     - Added micro and macro accuracies calculation

--- train.py
+++ train.py
@@ -4,7 +4,6 @@ import torch
 import torch.optim as optim
 import torch.nn as nn
 
-# from encoders.mean_encoder import MeanEncoder
 from encoders.encoding_helper import EncodingHelper
 
 from helpers.calculations_helper import CalculationsHelper
@@ -25,16 +24,14 @@ parameters_helper = ParametersHelper()
 parameters_helper.load_arguments()
 parameters_helper.print_arguments()
 
-# torch.set_default_tensor_type('torch.cuda.FloatTensor')
-device = torch.device("cuda:0")
+device = torch.device("cuda")
 
 # Load the data sets and the vocabulary
 print('Loading data...')
-data_storage = DataStorage()
+data_storage = DataStorage(parameters_helper.max_samples)
 
 token_vocabulary, _ = data_storage.get_vocabulary()
-train_iterator, dev_iterator, test_iterator = data_storage.get_dataset_iterators(
-    parameters_helper.batch_size, device)
+# train_iterator, dev_iterator, test_iterator = data_storage.get_dataset_iterators(parameters_helper.batch_size, device)
 dev_split_size = data_storage.get_dev_split_size()
 
 # Check if we can get a cached model. If not, create a new one
@@ -45,8 +42,7 @@ model = cache_storage.load_model_snapshot(parameters_helper.snapshot_location)
 if not model:
     encoding_helper = EncodingHelper()
     encoder = encoding_helper.get_encoder(parameters_helper.encoding_model)
-    model = SNLIClassifier(len(token_vocabulary), 300,
-                           encoder, token_vocabulary).to(device)
+    model = SNLIClassifier(encoder, token_vocabulary).to(device)
 
 # Initialize the loss and optimizer
 criterion = nn.CrossEntropyLoss()
@@ -60,7 +56,7 @@ best_dev_accuracy = -1
 
 print('Starting training...')
 
-statistics_helper = StatisticsHelper(train_batches_size=len(train_iterator))
+statistics_helper = StatisticsHelper()
 statistics_helper.print_header()
 
 plot_helper = PlotHelper()
@@ -69,34 +65,37 @@ current_learning_rate = parameters_helper.learning_rate
 
 # Start the training
 while current_learning_rate > minimum_learning_rate and epoch < parameters_helper.max_epochs:
-    train_iterator.init_epoch()
     n_correct, n_total = 0, 0
+
+    train_epoch_accuracies = []
+
+    train_iterator, dev_iterator, test_iterator = data_storage.get_dataset_iterators(
+        parameters_helper.batch_size, device)
 
     for batch_idx, batch in enumerate(train_iterator):
 
         # Switch the model to training mode
         model.train()
 
-        # Clear gradient accumulators
-        optimizer.zero_grad()
-
         # Make the forward pass
-        train_predictions = model(batch)
+        train_predictions = model.forward(batch)
 
         # Calculate the accuracy of the predictions in the current batch
         n_correct += calculations_helper.calculate_correct_predictions(
             train_predictions, batch.label)
 
         n_total += batch.batch_size
+
         train_accuracy = calculations_helper.calculate_full_accuracy(
             train_predictions, batch.label)
-
-        train_accuracy_normalized = calculations_helper.calculate_accuracy(
-            n_correct, n_total)
+        train_epoch_accuracies.append(train_accuracy)
 
         # calculate loss of the network output with respect to training labels
         train_loss = criterion(train_predictions, batch.label)
         plot_helper.add_train_result(train_accuracy, train_loss.item())
+
+        # Clear gradient accumulators
+        optimizer.zero_grad()
 
         # Make the backward pass
         train_loss.backward()
@@ -107,75 +106,79 @@ while current_learning_rate > minimum_learning_rate and epoch < parameters_helpe
         # Cache model
         if iterations % parameters_helper.save_every_steps == 0:
             cache_storage.save_snapshot(
-                model, iterations, train_accuracy_normalized, train_loss.item())
-
-        # Evaluate performance on the dev set periodically
-        if iterations % parameters_helper.evaluate_every_steps == 0:
-
-            # Switch model to evaluation mode
-            model.eval()
-            dev_iterator.init_epoch()
-
-            # Calculate the accuracy on the dev set
-            dev_correct_predictions, dev_loss = 0, 0
-            with torch.no_grad():
-                for dev_batch_idx, dev_batch in enumerate(dev_iterator):
-                    dev_predictions = model(dev_batch)
-                    dev_correct_predictions += calculations_helper.calculate_correct_predictions(
-                        dev_predictions, dev_batch.label)
-
-                    dev_loss = criterion(dev_predictions, dev_batch.label)
-
-            dev_accuracy = calculations_helper.calculate_accuracy(
-                dev_correct_predictions, dev_split_size)
-
-            statistics_helper.print_results(
-                epoch,
-                iterations,
-                batch_idx,
-                train_loss.item(),
-                train_accuracy_normalized,
-                dev_loss.item(),
-                dev_accuracy)
-
-            plot_helper.add_dev_result(dev_accuracy, dev_loss.item())
-            plot_helper.update_plot()
-
-            # Update best dev set accuracy if we have
-            # found a model with a better dev set accuracy
-            if dev_accuracy > best_dev_accuracy:
-                best_dev_accuracy = dev_accuracy
-                cache_storage.save_best_snapshot(
-                    model, iterations, dev_accuracy, dev_loss.item())
-            elif dev_accuracy < best_dev_accuracy:
-                print('Reducing learning rate...')
-                current_learning_rate /= 5.0
-                if current_learning_rate < minimum_learning_rate:
-                    print (f'Learning rate dropped below the minimum one({minimum_learning_rate}). Stopping training...')
-                    break
-
-                for g in optimizer.param_groups:
-                    g['lr'] = current_learning_rate
+                model, iterations, train_accuracy, train_loss.item())
 
         # Output statistics periodically
-        elif iterations % parameters_helper.log_every_steps == 0:
-            statistics_helper.print_results(
+        if iterations % parameters_helper.log_every_steps == 0:
+            train_micro_accuracy = calculations_helper.calculate_accuracy(
+                n_correct, n_total)
+            # plot_helper.plot_grad_flow(model.named_parameters())
+            statistics_helper.print_train_results(
                 epoch,
                 iterations,
                 batch_idx,
                 train_loss.item(),
-                train_accuracy_normalized)
+                train_micro_accuracy,
+                np.mean(train_epoch_accuracies),
+                train_batches_size=len(train_iterator))
 
         iterations += 1
+
+    train_micro_accuracy = calculations_helper.calculate_accuracy(
+        n_correct, n_total)
+    train_macro_accuracy = np.mean(train_epoch_accuracies)
+
+    # Switch model to evaluation mode
+    model.eval()
+
+    # Calculate the accuracy on the dev set
+    dev_correct_predictions, dev_loss = 0, 0
+    dev_epoch_accuracies = []
+    for dev_batch_idx, dev_batch in enumerate(dev_iterator):
+        dev_predictions = model.forward(dev_batch)
+        dev_correct_predictions += calculations_helper.calculate_correct_predictions(
+            dev_predictions, dev_batch.label)
+
+        dev_accuracy = calculations_helper.calculate_full_accuracy(
+            dev_predictions, dev_batch.label)
+
+        dev_epoch_accuracies.append(dev_accuracy)
+        dev_loss = criterion(dev_predictions, dev_batch.label)
+
+    dev_macro_accuracy = np.mean(dev_epoch_accuracies)
+    dev_micro_accuracy = calculations_helper.calculate_accuracy(
+        dev_correct_predictions, dev_split_size)
+
+    statistics_helper.print_epoch_results(
+        epoch,
+        iterations,
+        train_micro_accuracy,
+        train_macro_accuracy,
+        dev_loss.item(),
+        dev_micro_accuracy,
+        dev_macro_accuracy)
+
+    plot_helper.add_dev_result(dev_accuracy, dev_loss.item())
+    plot_helper.update_plot()
+
+    # Update best dev set accuracy if we have
+    # found a model with a better dev set accuracy
+    if dev_accuracy > best_dev_accuracy:
+        best_dev_accuracy = dev_accuracy
+        cache_storage.save_best_snapshot(
+            model, iterations, dev_accuracy, dev_loss.item())
+    elif dev_accuracy < best_dev_accuracy:
+        print('Reducing learning rate...')
+        current_learning_rate /= 5.0
+        if current_learning_rate < minimum_learning_rate:
+            print(
+                f'Learning rate dropped below the minimum one({minimum_learning_rate}). Stopping training...')
+            break
+
+        for g in optimizer.param_groups:
+            g['lr'] = current_learning_rate
 
     epoch += 1
 
 plot_helper.save_plot(
-    f'results/{parameters_helper.encoding_model}/result-final.png')
-
-# dev_accuracies = plot_helper.get_dev_accuracies()
-# dev_accuracies_sum = np.sum(dev_accuracies)
-# macro_accuracy =  dev_accuracies_sum / len(dev_accuracies)
-# micro_accuracy = dev_accuracies_sum / dev_split_size
-
-# print(f'Macro accuracy: {}')
+    f'results/{parameters_helper.encoding_model}/result-final.pickle')
