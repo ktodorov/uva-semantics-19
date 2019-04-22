da9d44625c0bb680bc5774bc5d2bc9cf62a4fe5e
ktodorov <kztodorov@outlook.com>
2019-04-22 17:21:00 +0200

    Fixed LSTM encoders padding issues

--- encoders/lstm_encoder.py
+++ encoders/lstm_encoder.py
@@ -13,14 +13,13 @@ class LSTMEncoder(BaseEncoder):
             max_pooling=False):
         super(LSTMEncoder, self).__init__()
 
-        self._input_dimensions = 4*2048
+        self._input_dimensions = 4*hidden_dim
 
         if bidirectional:
             self._input_dimensions *= 2
 
         self.embedding_dim = embedding_dim
         self.hidden_dim = hidden_dim
-        self.batch_first = False
         self.output_size = hidden_dim
         self.bidirectional = bidirectional
         self.max_pooling = max_pooling
@@ -30,7 +29,6 @@ class LSTMEncoder(BaseEncoder):
 
         self.model = nn.LSTM(input_size=self.embedding_dim,
                              hidden_size=self.hidden_dim,
-                             batch_first=self.batch_first,
                              bidirectional=bidirectional)
 
     def forward(self, x, len_x):
@@ -42,24 +40,19 @@ class LSTMEncoder(BaseEncoder):
 
         x_select = x.index_select(1, Variable(idx))
 
-        # Handling padding in Recurrent Networks
-        x_packed = nn.utils.rnn.pack_padded_sequence(x_select, len_x_sorted, batch_first=False)
+        x_packed = nn.utils.rnn.pack_padded_sequence(x_select, len_x_sorted)
         x_output = self.model(x_packed)[0]
 
-        x_output = nn.utils.rnn.pad_packed_sequence(x_output, batch_first=False)[0]
+        x_output = nn.utils.rnn.pad_packed_sequence(x_output)[0]
 
         if self.max_pooling:
-            x_output, _ = torch.max(out, dim=0)
+            x_output, _ = torch.max(x_output, dim=0)
             return x_output
 
-        x_output = x_output.view(-1, self.output_size)
-        
-        longest_sentence = max(len_x)
-        last_word = torch.Tensor([i*longest_sentence + len_x[i] - 1 for i in range(len(len_x))]).long().cuda()
-        x_output = x_output.index_select(0, Variable(last_word))
+        original_indices = torch.tensor([idx[i] for i in idx]).cuda()
+        x_output = x_output.index_select(1, Variable(original_indices))
 
-        _, idx = torch.sort(idx, 0, descending=False)
-
-        x_output = x_output.index_select(0, Variable(idx))
+        unsorted_lens = len_x_sorted.index_select(0, original_indices)
+        x_output = torch.stack([x_output[item_length - 1, i, :] for i, item_length in enumerate(unsorted_lens)])
 
         return x_output
